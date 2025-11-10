5827cd6de709e21630f4b8e3dd1db8464b21bc07
Jonatan Helander <Jonatanhelander@hotmail.com>
2025-11-10 20:05:45 +0100

    Added second carousel to pad out landing page content

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,11 +4,49 @@ import Carousel from "./components/Carousel";
 import Hero from "./components/Hero";
 
 export default async function HomePage() {
-  const products = serializeProducts(await prisma.product.findMany());
+  const mensWatches = serializeProducts(
+    await prisma.product.findMany({
+      where: {
+        productCategory: {
+          some: {
+            category: {
+              name: "mens-watches",
+            },
+          },
+        },
+      },
+    })
+  );
+
+  const womensWatches = serializeProducts(
+    await prisma.product.findMany({
+      where: {
+        productCategory: {
+          some: {
+            category: {
+              name: "womens-watches",
+            },
+          },
+        },
+      },
+    })
+  );
+
   return (
     <main className="gap-12 pb-12">
       <Hero />
-      <Carousel props={products} />
+      <div className="flex self-start pl-6 md:pl-16 xl:pl-32">
+        <span className="text-xl md:text-3xl font-bold text-nowrap">
+          The hottest products for him
+        </span>
+      </div>
+      <Carousel props={mensWatches} />
+      <div className="flex self-start pl-6 md:pl-16 xl:pl-32">
+        <span className="text-xl md:text-3xl font-bold text-nowrap">
+          The finest assortment for her
+        </span>
+      </div>
+      <Carousel props={womensWatches} />
     </main>
   );
 }
